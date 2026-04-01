b4a3c0777d2c94bfa35ed3dc882391d3d36fe47d
Adijata <adijatadjogo91@gmail.com>
2026-04-01 16:47:37 +0200

    Fix za redirects

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
+```
+
+Zatim u `public` folderu otvori `_redirects` fajl i provjeri da li ima tačno ovaj sadržaj bez ikakvih dodatnih razmaka ili praznih redova:
+```
+/*    /index.html   200
