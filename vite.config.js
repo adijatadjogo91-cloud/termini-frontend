d215815beb1e796bf75c7ac7a1e17e2442fe4757
Adijata <adijatadjogo91@gmail.com>
2026-04-26 20:22:09 +0200

    Fix: force novi bundle hash

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+      }
+    }
   }
 })
